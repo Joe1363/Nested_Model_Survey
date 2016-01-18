33b27efe2173d773fc0ef3da926bd01c8e7ecb30
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-18 11:32:01 -0800

    created questions model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118192556) do
+ActiveRecord::Schema.define(version: 20160118193032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "survey_id"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.string   "name"
