0fe4c2b75613bf9e96e572b328f678942be7dba0
Joseph Cabral <joe1363_2001@yahoo.com>
2016-01-20 13:16:43 -0800

    added spec for edit survey

--- spec/features/edit_survey_spec.rb
+++ spec/features/edit_survey_spec.rb
@@ -5,18 +5,43 @@ describe 'Edit Survey' do
     visit '/'
     click_link 'New Survey'
 
-    fill_in "Name", :with => "Math Surveys"
+    fill_in "Name", :with => "Math Survey"
     click_link "Add Question"
     fill_in "Question", :with => "What is 2 + 2?"
     click_link "Add Answer"
-    fill_in "Answer", :with => "2"
+    fill_in "Answer", :with => "bacon"
     click_button "Create Survey"
 
     visit '/'
   end
 
   it 'should edit an existing survey', :js => true do
-    expect(page).to have_selector(:link_or_button, "Math Surveys")
+    expect(page).to have_selector(:link_or_button, "Math Survey")
+    click_link "Math Survey"
+
+    expect(page).to have_selector(:link_or_button, "Edit")
+    click_link "Edit"
+
+    fill_in "Question", :with => "What is 2 + 2??"
+    click_link "Remove"
+    click_link "Add Answer"
+    fill_in "Answer", :with => "4"
+
+    click_link "Add Question"
+
+    within all('.qArea').last do
+      fill_in ('Question'), :with => 'What is 3 + 3?'
+      click_link "Add Answer"
+      fill_in ('Answer'), :with => '6'
+    end
+
+    click_button "Update Survey"
+
+    expect(page).to have_content "What is 2 + 2??"
+    expect(page).to have_content "4"
+    expect(page).not_to have_content "bacon"
+    expect(page).to have_content "What is 3 + 3?"
+    # expect(page).to have_content "6"
 
   end
 end
